7a3134d96b24be8db112e2119cd27d22232e44b7
Ryan Kim <ryank9462@gmail.com>
2021-02-01 14:50:23 -0600

    prototype coin counter

--- App.js
+++ App.js
@@ -1,36 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [pennyCount, setPennyCount] = useState(0);
+  const [nickelCount, setNickelCount] = useState(0);
+  const [dimeCount, setDimeCount] = useState(0);
+  const [quarterCount, setQuarterCount] = useState(0);
+
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <Text>Pennies X </Text>
-        <TextInput style={styles.input}></TextInput>
+        <TextInput onChangeText={number => setPennyCount(number)} style={styles.input}></TextInput>
         <Text> = </Text>
-        <TextInput style={styles.input}></TextInput>
+        <Text>$ {(pennyCount * 0.01).toFixed(2)}</Text>
       </View>
 
       <View style={styles.row}>
         <Text>Nickels X </Text>
-        <TextInput style={styles.input}></TextInput>
+        <TextInput onChangeText={number => setNickelCount(number)} style={styles.input}></TextInput>
         <Text> = </Text>
-        <TextInput style={styles.input}></TextInput>
+        <Text>$ {(nickelCount * 0.05).toFixed(2)}</Text>
       </View>
 
       <View style={styles.row}>
         <Text>Dimes X </Text>
-        <TextInput style={styles.input}></TextInput>
+        <TextInput onChangeText={number => setDimeCount(number)} style={styles.input}></TextInput>
         <Text> = </Text>
-        <TextInput style={styles.input}></TextInput>
+        <Text>$ {(dimeCount * 0.10).toFixed(2)}</Text>
       </View>
 
       <View style={styles.row}>
         <Text>Quarters X </Text>
-        <TextInput style={styles.input}></TextInput>
+        <TextInput onChangeText={number => setQuarterCount(number)} style={styles.input}></TextInput>
         <Text> = </Text>
-        <TextInput style={styles.input}></TextInput>
+        <Text>$ {(quarterCount * 0.25).toFixed(2)}</Text>
+      </View>
+
+      <View style={styles.row}>
+        <Text>Coin Total = </Text>
+        <Text>$ {(pennyCount * 0.01 + nickelCount * 0.05 + dimeCount * 0.10 + quarterCount * 0.25).toFixed(2)}</Text>
       </View>
 
       <StatusBar style="auto" />
@@ -50,10 +61,11 @@ const styles = StyleSheet.create({
     borderWidth: 2,  // size/width of the border
     borderColor: 'lightgrey',  // color of the border
     paddingLeft: 10,
-    marginLeft: 10
+    marginLeft: 10,
   },
 
   row: {
-    flexDirection: 'row'
+    flexDirection: 'row',
+    marginBottom: 20,
   }
 });
