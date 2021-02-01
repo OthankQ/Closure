728b0f2ef4361e9ba7e1b8632192c92e9ed79b15
Ryan Kim <ryank9462@gmail.com>
2021-02-01 14:33:06 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.row}>
+        <Text>Pennies X </Text>
+        <TextInput style={styles.input}></TextInput>
+        <Text> = </Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
+      <View style={styles.row}>
+        <Text>Nickels X </Text>
+        <TextInput style={styles.input}></TextInput>
+        <Text> = </Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
+      <View style={styles.row}>
+        <Text>Dimes X </Text>
+        <TextInput style={styles.input}></TextInput>
+        <Text> = </Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
+      <View style={styles.row}>
+        <Text>Quarters X </Text>
+        <TextInput style={styles.input}></TextInput>
+        <Text> = </Text>
+        <TextInput style={styles.input}></TextInput>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +45,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    borderWidth: 2,  // size/width of the border
+    borderColor: 'lightgrey',  // color of the border
+    paddingLeft: 10,
+    marginLeft: 10
+  },
+
+  row: {
+    flexDirection: 'row'
+  }
 });
